8a8eaf84f841da3699861a93abd8b06855e0f308
хаюхай <nikolamoscow2005@gmail.com>
2026-04-22 13:03:57 +0400

    added hw2 solutions

--- src/test/java/edu/phystech/hw2/IntHolderTest.java
+++ src/test/java/edu/phystech/hw2/IntHolderTest.java
@@ -10,21 +10,55 @@ class IntHolder {
     private int value;
 
     public int getValue() {
-        return 0;
+        return value;
     }
 
-    public void swap(IntHolder other) {}
+    public void swap(IntHolder other) {
+        int tmp = value;
+        value = other.value;
+        other.value = tmp;
+    }
 
-    public IntHolder(int value) {}
+    public IntHolder(int value) {
+        this.value = value;
+    }
 
-    public static IntHolder valueOf(int x) { return null; }
+    public static IntHolder valueOf(int x) {
+        return new IntHolder(x);
+    }
 
-    public IntHolder plus(IntHolder rhv) { return null; }
+    public IntHolder plus(IntHolder rhv) {
+        return new IntHolder(value + rhv.value);
+    }
 
-    public IntHolder minus(IntHolder rhv) { return null; }
+    public IntHolder minus(IntHolder rhv) {
+        return new IntHolder(value - rhv.value);
+    }
 
-    public IntHolder times(IntHolder rhv) { return null; }
-    public IntHolder div(IntHolder rhv) { return null; }
+    public IntHolder times(IntHolder rhv) {
+        return new IntHolder(value * rhv.value);
+    }
+
+    public IntHolder div(IntHolder rhv) {
+        return new IntHolder(value / rhv.value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        IntHolder intHolder = (IntHolder) o;
+        return value == intHolder.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 
 }
 
